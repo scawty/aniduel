a6cb637854ba995c305f5a7d7ec74e47f72da1a3
scawty <cilentow@gmail.com>
2023-07-01 21:47:14 -0500

    added refetch for characters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,19 @@ import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 import Image from "next/image";
+import { useState } from "react";
+import { type Character } from "@prisma/client";
 
 export default function Home() {
-  const matchup = api.matchups.getMatchup.useQuery();
+  const { data: matchup, refetch } = api.matchups.getMatchup.useQuery();
 
+  const handleMatchupResult = () => {
+    refetch()
+      .then()
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <Head>
@@ -31,58 +40,60 @@ export default function Home() {
             <div className="flex h-4/5 w-3/4 flex-row justify-evenly">
               <div className="m-4 h-fit hover:scale-110">
                 <Image
-                  src={matchup?.data?.[0]?.imgUrl || ""}
-                  alt={matchup?.data?.[0]?.name || "image1"}
+                  src={matchup?.[0]?.imgUrl || ""}
+                  alt={matchup?.[0]?.name || "image1"}
                   width={200}
                   height={200}
                   className="rounded-lg"
+                  onClick={handleMatchupResult}
                 ></Image>
-                <h2 className="text-center text-xl">
-                  {matchup?.data?.[0]?.name}
-                </h2>
+                <h2 className="text-center text-xl">{matchup?.[0]?.name}</h2>
                 <div className="flex w-full justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6 hover:cursor-pointer"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-                    />
-                  </svg>
+                  <a href={matchup?.[0]?.malUrl} target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6 hover:cursor-pointer"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                      />
+                    </svg>
+                  </a>
                 </div>
               </div>
               <div className="m-4 h-fit hover:scale-110">
                 <Image
-                  src={matchup?.data?.[1]?.imgUrl || ""}
-                  alt={matchup?.data?.[1]?.name || "image2"}
+                  src={matchup?.[1]?.imgUrl || ""}
+                  alt={matchup?.[1]?.name || "image2"}
                   width={200}
                   height={200}
                   className="rounded-lg"
+                  onClick={handleMatchupResult}
                 ></Image>
-                <h2 className="text-center text-xl">
-                  {matchup?.data?.[1]?.name}
-                </h2>
+                <h2 className="text-center text-xl">{matchup?.[1]?.name}</h2>
                 <div className="flex w-full justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6 hover:cursor-pointer"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-                    />
-                  </svg>
+                  <a href={matchup?.[1]?.malUrl} target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6 hover:cursor-pointer"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                      />
+                    </svg>
+                  </a>
                 </div>
               </div>
             </div>
