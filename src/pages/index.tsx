630b60098d6f2149df56eff431a345dce1bd13a7
scawty <cilentow@gmail.com>
2023-07-01 19:51:29 -0500

    updated character card styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,17 +28,34 @@ export default function Home() {
           </header>
           <div className="flex h-full w-full flex-col items-center justify-center">
             <h2 className="m-2 text-2xl">Choose your favorite</h2>
-            <div className="flex h-4/5 w-4/5 flex-row justify-evenly">
+            <div className="flex h-4/5 w-3/4 flex-row justify-evenly">
               <div className="m-4 h-fit hover:scale-110">
                 <Image
                   src={matchup?.data?.[0]?.imgUrl || ""}
                   alt={matchup?.data?.[0]?.name || "image1"}
                   width={200}
                   height={200}
+                  className="rounded-lg"
                 ></Image>
                 <h2 className="text-center text-xl">
                   {matchup?.data?.[0]?.name}
                 </h2>
+                <div className="flex w-full justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6 hover:cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                    />
+                  </svg>
+                </div>
               </div>
               <div className="m-4 h-fit hover:scale-110">
                 <Image
@@ -46,10 +63,27 @@ export default function Home() {
                   alt={matchup?.data?.[1]?.name || "image2"}
                   width={200}
                   height={200}
+                  className="rounded-lg"
                 ></Image>
                 <h2 className="text-center text-xl">
                   {matchup?.data?.[1]?.name}
                 </h2>
+                <div className="flex w-full justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6 hover:cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
