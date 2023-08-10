727581bf982a39f790c35c2babc798f8484281d0
scawty <cilentow@gmail.com>
2023-08-10 16:04:39 -0500

    changed matchup state to support lingering scores on matchup result

--- src/components/Matchup.tsx
+++ src/components/Matchup.tsx
@@ -7,33 +7,41 @@ const Matchup = () => {
   const { data: matchup, refetch } = api.matchups.getMatchup.useQuery();
   const mutation = api.matchups.postMatchupResult.useMutation();
 
-  const [matchupState, setMatchupState] = useState<{
-    isComplete: boolean;
-    winnerId: number | null;
-    loserId: number | null;
-  }>({
-    isComplete: false,
-    winnerId: null,
-    loserId: null,
-  });
+  const [matchupComplete, setMatchupComplete] = useState<boolean>(false);
+  const [lingeringScores, setLingeringScores] = useState<number[]>([0, 0]);
+  const [winner, setWinner] = useState<"CharacterOne" | "CharacterTwo" | null>(
+    null
+  );
 
-  const handleMatchupResult = (winner: number, loser: number) => {
-    setMatchupState({
-      isComplete: true,
-      winnerId: winner,
-      loserId: loser,
-    });
+  if (!matchup) {
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <h2 className="m-2 text-2xl">Who do you rank higher?</h2>
+        <div className="flex h-4/5 w-3/4 flex-row justify-evenly">
+          <MatchupCard isLoading={true} />
+          <MatchupCard isLoading={true} />
+        </div>
+      </div>
+    );
+  }
+
+  const handleMatchupResult = (
+    winner: number,
+    loser: number,
+    winnerName: "CharacterOne" | "CharacterTwo"
+  ) => {
+    setMatchupComplete(true);
+
+    setLingeringScores([matchup[0]?.elo ?? 0, matchup[1]?.elo ?? 0]);
+
+    setWinner(winnerName);
 
     mutation.mutate(
       { winnerId: winner, loserId: loser },
       {
         onSuccess: () => {
           setTimeout(() => {
-            setMatchupState({
-              isComplete: false,
-              winnerId: winner,
-              loserId: loser,
-            });
+            setMatchupComplete(false);
           }, 800);
           refetch()
             .then()
@@ -54,70 +62,68 @@ const Matchup = () => {
   };
   return (
     <>
-      {matchup ? (
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <h2 className="m-2 text-2xl">Who do you rank higher?</h2>
-          <div className="flex h-8 w-full flex-row justify-evenly">
-            <EloScore
-              show={matchupState.isComplete}
-              score={matchup[0]?.elo}
-              isWinner={matchup[0]?.id == matchupState.winnerId}
-            />
-            <EloScore
-              show={matchupState.isComplete}
-              score={matchup[1]?.elo}
-              isWinner={matchup[1]?.id == matchupState.winnerId}
-            />
-          </div>
-          <div className="flex h-4/5 w-3/4 flex-col ">
-            <div className="flex h-fit w-full flex-row justify-evenly">
-              {matchup[0] ? (
-                <MatchupCard
-                  handleMatchupResult={() => {
-                    if (matchup[0]?.id && matchup[1]?.id) {
-                      handleMatchupResult(matchup[0].id, matchup[1].id);
-                    }
-                  }}
-                  isLoading={false}
-                  character={matchup[0]}
-                />
-              ) : (
-                <MatchupCard isLoading={true} />
-              )}
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <h2 className="m-2 text-2xl">Who do you rank higher?</h2>
+        <div className="flex h-8 w-full flex-row justify-evenly">
+          <EloScore
+            show={matchupComplete}
+            score={lingeringScores[0]}
+            isWinner={winner === "CharacterOne"}
+          />
+          <EloScore
+            show={matchupComplete}
+            score={lingeringScores[1]}
+            isWinner={winner === "CharacterTwo"}
+          />
+        </div>
+        <div className="flex h-4/5 w-3/4 flex-col ">
+          <div className="flex h-fit w-full flex-row justify-evenly">
+            {matchup[0] ? (
+              <MatchupCard
+                handleMatchupResult={() => {
+                  if (matchup[0]?.id && matchup[1]?.id) {
+                    handleMatchupResult(
+                      matchup[0].id,
+                      matchup[1].id,
+                      "CharacterOne"
+                    );
+                  }
+                }}
+                isLoading={false}
+                character={matchup[0]}
+              />
+            ) : (
+              <MatchupCard isLoading={true} />
+            )}
 
-              {matchup[1] ? (
-                <MatchupCard
-                  handleMatchupResult={() => {
-                    if (matchup[1]?.id && matchup[0]?.id) {
-                      handleMatchupResult(matchup[1].id, matchup[0].id);
-                    }
-                  }}
-                  isLoading={false}
-                  character={matchup[1]}
-                />
-              ) : (
-                <MatchupCard isLoading={true} />
-              )}
-            </div>
-            <div className="flex w-full flex-row justify-center">
-              <button
-                className="rounded-md bg-indigo-800 px-4 py-2 text-xl font-semibold shadow hover:bg-indigo-700"
-                onClick={handleSkip}
-              >
-                Skip
-              </button>
-            </div>
+            {matchup[1] ? (
+              <MatchupCard
+                handleMatchupResult={() => {
+                  if (matchup[1]?.id && matchup[0]?.id) {
+                    handleMatchupResult(
+                      matchup[1].id,
+                      matchup[0].id,
+                      "CharacterTwo"
+                    );
+                  }
+                }}
+                isLoading={false}
+                character={matchup[1]}
+              />
+            ) : (
+              <MatchupCard isLoading={true} />
+            )}
+          </div>
+          <div className="flex w-full flex-row justify-center">
+            <button
+              className="rounded-md bg-indigo-800 px-4 py-2 text-xl font-semibold shadow hover:bg-indigo-700"
+              onClick={handleSkip}
+            >
+              Skip
+            </button>
           </div>
         </div>
-      ) : (
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <h2 className="m-2 text-2xl">Who do you rank higher?</h2>
-          <div className="flex h-4/5 w-3/4 flex-row justify-evenly">
-            <MatchupCard isLoading={true} />
-            <MatchupCard isLoading={true} />
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 };
